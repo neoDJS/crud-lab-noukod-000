f7794eae3bfacd32558445abe2d6d7f078858a60
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-11 12:00:00 +0000

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
-  renderReviews = this.props.reviews.map(review => <Review restoId={this.props.restoId} key={review.id} {...review} delete={this.props.delete}/>)
+  renderReviews = this.props.reviews.map(review => <Review review={review} restoId={this.props.restoId} key={review.id} delete={this.props.delete}/>)
   render() {
     return (
       <ul>
